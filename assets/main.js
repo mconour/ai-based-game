ad6a5a546c4b8a6f656175705c1ceae18e3232a5
Michael Conour <michaelconour@gmail.com>
2020-09-01 17:36:03 -0700

    complete doAiMove function

--- assets/main.js
+++ assets/main.js
@@ -90,7 +90,7 @@ playGame = () => {
 doRandom = () => {
     if (count % 2 !== 0 && possibleMoves.length > 0) {
         aiMove = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
-        var move = document.getElementById(aiMove);
+        let move = document.getElementById(aiMove);
         move.innerHTML = computerLetter;
         square.push(aiMove);
         count++;
@@ -118,10 +118,10 @@ computerO = () => {
 	} else if (/(1)|(3)|(7)|(9)/.test(squareX) && squareX.indexOf('5') === 0 && squareX.length === 2) {
 		possibleMoves = ['1', '3', '7', '9'];
 
-		var index = possibleMoves.indexOf(squareX[1]);
+		let index = possibleMoves.indexOf(squareX[1]);
 		possibleMoves.splice(index, 1);
 
-		var aiTaken = possibleMoves.indexOf(squareO[0]);
+		let aiTaken = possibleMoves.indexOf(squareO[0]);
 		possibleMoves.splice(aiTaken, 1);
 
 		pushSquare(squareO, squareX);
@@ -137,9 +137,9 @@ computerO = () => {
 
 computerX = () => {
 
-	var oppositeCorner = (10 - squareX[0]).toString();
-	var firstAiMove = possibleMoves.indexOf(squareX[0]);
-	var indexOfOpposite = possibleMoves.indexOf(oppositeCorner);
+	let oppositeCorner = (10 - squareX[0]).toString();
+	let firstAiMove = possibleMoves.indexOf(squareX[0]);
+	let indexOfOpposite = possibleMoves.indexOf(oppositeCorner);
 	possibleMoves = [];
 	
 	Array.prototype.forEach.call(col, function(el, i){
@@ -173,4 +173,118 @@ computerX = () => {
 	}
 
 	checkWin(squareX, 'Computer wins', true, false);
+}
+
+doAiMove = (square) => {
+    const conditions = {
+		rows: {
+			a: /(?=.*1)(?=.*2)/,
+			b: /(?=.*1)(?=.*3)/,
+			c: /(?=.*2)(?=.*3)/,
+			d: /(?=.*4)(?=.*5)/,
+			e: /(?=.*4)(?=.*6)/,
+			f: /(?=.*5)(?=.*6)/,
+			g: /(?=.*7)(?=.*8)/,
+			h: /(?=.*7)(?=.*9)/,
+			i: /(?=.*8)(?=.*9)/
+		},
+		columns: {
+			a: /(?=.*1)(?=.*4)/,
+			b: /(?=.*1)(?=.*7)/,
+			c: /(?=.*4)(?=.*7)/,
+			d: /(?=.*2)(?=.*5)/,
+			e: /(?=.*2)(?=.*8)/,
+			f: /(?=.*5)(?=.*8)/,
+			g: /(?=.*3)(?=.*6)/,
+			h: /(?=.*3)(?=.*9)/,
+			i: /(?=.*6)(?=.*9)/
+		},
+		diagonals: {
+			a: /(?=.*1)(?=.*5)/,
+			b: /(?=.*1)(?=.*9)/,
+			c: /(?=.*5)(?=.*9)/,
+			d: /(?=.*3)(?=.*5)/,
+			e: /(?=.*3)(?=.*7)/,
+			f: /(?=.*5)(?=.*7)/
+		}
+	}
+
+	let squareString = square.join();
+
+	if (count % 2 !== 0) {
+
+		if (conditions.rows.a.test(squareString) && possibleMoves.indexOf('3') > -1) {
+			aiMove = '3';
+			count++;
+		} else if (conditions.rows.b.test(squareString) && possibleMoves.indexOf('2') > -1) {
+			aiMove = '2';
+			count++;
+		} else if (conditions.rows.c.test(squareString) && possibleMoves.indexOf('1') > -1) {
+			aiMove = '1';
+			count++;
+		} else if (conditions.rows.d.test(squareString) && possibleMoves.indexOf('6') > -1) {
+			aiMove = '6';
+			count++;
+		} else if (conditions.rows.e.test(squareString) && possibleMoves.indexOf('5') > -1) {
+			aiMove = '5';
+			count++;
+		} else if (conditions.rows.f.test(squareString) && possibleMoves.indexOf('4') > -1) {
+			aiMove = '4';
+			count++;
+		} else if (conditions.rows.g.test(squareString) && possibleMoves.indexOf('9') > -1) {
+			aiMove = '9';
+			count++;
+		} else if (conditions.rows.h.test(squareString) && possibleMoves.indexOf('8') > -1) {
+			aiMove = '8';
+			count++;
+		} else if (conditions.rows.i.test(squareString) && possibleMoves.indexOf('7') > -1) {
+			aiMove = '7';
+			count++;
+		} else if (conditions.columns.a.test(squareString) && possibleMoves.indexOf('7') > -1) {
+			aiMove = '7';
+			count++;
+		} else if (conditions.columns.b.test(squareString) && possibleMoves.indexOf('4') > -1) {
+			aiMove = '4';
+			count++;
+		} else if (conditions.columns.c.test(squareString) && possibleMoves.indexOf('1') > -1) {
+			aiMove = '1';
+			count++;
+		} else if (conditions.columns.d.test(squareString) && possibleMoves.indexOf('8') > -1) {
+			aiMove = '8';
+			count++;
+		} else if (conditions.columns.e.test(squareString) && possibleMoves.indexOf('5') > -1) {
+			aiMove = '5';
+			count++;
+		} else if (conditions.columns.f.test(squareString) && possibleMoves.indexOf('2') > -1) {
+			aiMove = '2';
+			count++;
+		} else if (conditions.columns.g.test(squareString) && possibleMoves.indexOf('9') > -1) {
+			aiMove = '9';
+			count++;
+		} else if (conditions.columns.h.test(squareString) && possibleMoves.indexOf('6') > -1) {
+			aiMove = '6';
+			count++;
+		} else if (conditions.columns.i.test(squareString) && possibleMoves.indexOf('3') > -1) {
+			aiMove = '3';
+			count++;
+		} else if (conditions.diagonals.a.test(squareString) && possibleMoves.indexOf('9') > -1) {
+			aiMove = '9';
+			count++;
+		} else if (conditions.diagonals.b.test(squareString) && possibleMoves.indexOf('5') > -1) {
+			aiMove = '5';
+			count++;
+		} else if (conditions.diagonals.c.test(squareString) && possibleMoves.indexOf('1') > -1) {
+			aiMove = '1';
+			count++;
+		} else if (conditions.diagonals.d.test(squareString) && possibleMoves.indexOf('7') > -1) {
+			aiMove = '7';
+			count++;
+		} else if (conditions.diagonals.e.test(squareString) && possibleMoves.indexOf('5') > -1) {
+			aiMove = '5';
+			count++;
+		} else if (conditions.diagonals.f.test(squareString) && possibleMoves.indexOf('3') > -1) {
+			aiMove = '3';
+			count++;
+		}
+	}
 }
